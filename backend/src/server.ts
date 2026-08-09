010e0beeaf65c35667ce79d1c13deb0d22f681ba
SpearShard <gjayaditya20@gmail.com>
2026-08-09 14:51:23 +0530

    i fixed server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import app from "./app";
-import prisma from "./config/database";
+import app from "./app.js";
+import prisma from "./config/database.js";
 
 dotenv.config();
 
